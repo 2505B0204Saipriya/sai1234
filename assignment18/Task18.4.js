bb20c946e7e647bc8b479a6d29312a253b608977
2505B0204Saipriya <2505B04204@sru.edu.in>
2025-12-09 23:48:26 +0530

    Delete assignment18/Task18.4.js

--- assignment18/Task18.4.js
+++
@@ -1,8 +0,0 @@
-function printStudents(students) {
-    console.log("Student List:");
-    for (let name of students) {
-        console.log(name);
-    }
-}
-
-printStudents(["Alice", "Bob", "Charlie"]);
